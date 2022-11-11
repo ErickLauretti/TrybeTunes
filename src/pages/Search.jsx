e4761f0a989cb1475f46ecec668cd4903d222d64
Erick Lauretti <erick.nighyt@hotmail.com>
2022-11-11 18:08:43 -0300

    requisito 5 ok

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,52 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    disabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.areDisable(value);
+    });
+  };
+
+  areDisable = (name) => {
+    const numero = 2;
+    this.setState({
+      disabled: (name.length < numero),
+    });
+  };
+
   render() {
+    const { state: {
+      disabled }, handleChange,
+    } = this;
+
     return (
       <div data-testid="page-search">
-        Search
         <Header />
+        <form>
+          <label htmlFor="search">
+            <input
+              type="text"
+              name="search"
+              data-testid="search-artist-input"
+              placeholder="Nome do Artista"
+              onChange={ handleChange }
+            />
+          </label>
+        </form>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ disabled }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
